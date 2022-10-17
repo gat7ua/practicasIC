e1cf80995097a8d2a9cc72120954f15a2c00f8e9
fapm4 <fapm4@alu.ua.es>
2022-10-18 00:32:17 +0200

    Falta crear gráficas

--- Practica2/canny-git/main.cpp
+++ Practica2/canny-git/main.cpp
@@ -25,10 +25,18 @@ double sig;
 int main(int argc, char **argv)
 {
 	// Exit program if proper arguments are not provided by user
+	bool activaBucle = false;
+	int max_threshold = 1, max_value = 1;
 	if (argc != 4)
 	{
-		cout << "Proper syntax: ./a.out <input_filename> <high_threshold> <sigma_value>" << endl;
-		return 0;
+		cout << "Entrando en modo automático" << endl;
+		cout << "A continuación se ejecturá el algoritmo con distintos valores para el estudio del tiempo" << endl;
+		activaBucle = true;
+		max_threshold = 20;
+		max_value = 20;
+
+		// cout << "Proper syntax: ./a.out <input_filename> <high_threshold> <sigma_value>" << endl;
+		// return 0;
 	}
 
 	// Exit program if file doesn't open
@@ -41,99 +49,123 @@ int main(int argc, char **argv)
 		return 0;
 	}	
 
-	auto start = std::chrono::high_resolution_clock::now();
-	// Opening output files
-	ofstream img1("./output_images/canny_mag.pgm", ios::binary);
-	ofstream img2("./output_images/canny_peaks.pgm", ios::binary);		
-	ofstream img3("./output_images/canny_final.pgm", ios::binary);
+	cout << "Resultados de la ejecución" << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Threshold \t Sigma value \t Tiempo de ejecución" << endl;
 
-	::hi = stoi(argv[2]);
-	::lo = .35 * hi;
-	::sig = stoi(argv[3]);
+	for(int z = 1; z <= max_threshold; z++){
+		for(int t = 1; t <= max_value; t++){
+			try{
+				auto start = std::chrono::high_resolution_clock::now();
+				// Opening output files
+				ofstream img1("./output_images/canny_mag.pgm", ios::binary);
+				ofstream img2("./output_images/canny_peaks.pgm", ios::binary);		
+				ofstream img3("./output_images/canny_final.pgm", ios::binary);
 
-	// Storing header information and copying into the new ouput images
-	infile >> ::type >> ::width >> ::height >> ::intensity;
-	img1 << type << endl << width << " " << height << endl << intensity << endl;
-	img2 << type << endl << width << " " << height << endl << intensity << endl;
-	img3 << type << endl << width << " " << height << endl << intensity << endl;
+				if(activaBucle == true){
+					::hi = z;
+					::lo = .35 * hi;
+					::sig = t;
+				}
+				else{
+					::hi = stoi(argv[2]);
+					::lo = .35 * hi;
+					::sig = stoi(argv[3]);
+				}
 
-	// These matrices will hold the integer values of the input image and masks.
-	// I'm dynamically allocating arrays to easily pass them into functions.
-	double **pic = new double*[height], **mag = new double*[height], **final = new double*[height];
-	double **x = new double*[height], **y = new double*[height];
+				// Storing header information and copying into the new ouput images
+				infile >> ::type >> ::width >> ::height >> ::intensity;
+				img1 << type << endl << width << " " << height << endl << intensity << endl;
+				img2 << type << endl << width << " " << height << endl << intensity << endl;
+				img3 << type << endl << width << " " << height << endl << intensity << endl;
 
-	for (int i = 0; i < height; i++)
-	{
-		pic[i] = new double[width];
-		mag[i] = new double[width];
-		final[i] = new double[width];
-		x[i] = new double[width];
-		y[i] = new double[width];
+				// These matrices will hold the integer values of the input image and masks.
+				// I'm dynamically allocating arrays to easily pass them into functions.
+				double **pic = new double*[height], **mag = new double*[height], **final = new double*[height];
+				double **x = new double*[height], **y = new double*[height];
+
+				for (int i = 0; i < height; i++)
+				{
+					pic[i] = new double[width];
+					mag[i] = new double[width];
+					final[i] = new double[width];
+					x[i] = new double[width];
+					y[i] = new double[width];
+				}
+
+				// Reading in the input image as integers
+				for (int i = 0; i < height; i++)
+					for (int j = 0; j < width; j++)
+						pic[i][j] = (int)infile.get();
+
+				// Create the magniute matrix
+				try{
+					magnitude_matrix(pic, mag, x, y);
+				}
+				catch(const exception& er){
+					continue;
+				}
+				// Get all the peaks and store them in vector
+				HashMap *peaks = new HashMap();
+				vector<Point*> v = peak_detection(mag, peaks, x, y);
+
+				// Go through the vector and call the recursive function and each point. If the value
+				// in the mag matrix is hi, then immediately accept it in final. If lo, then immediately
+				// reject. If between lo and hi, then check if it's next to a hi pixel using recursion
+				HashMap *h = new HashMap();
+				int a, b;
+				for (int i = 0; i < v.size(); i++)
+				{
+					a = v.at(i)->x;
+					b = v.at(i)->y;
+
+					if (mag[a][b] >= hi)
+						final[a][b] = 255;
+					else if (mag[a][b] < lo)
+						final[a][b] = 0;
+					else
+						recursiveDT(mag, final, h, peaks, a, b, 0);
+				}
+
+				// ================================= IMAGE OUTPUT =================================
+				// Outputting the 'mag' matrix to img1. It's very important to cast it to a char.
+				// To make sure that the decimal doesn't produce any wonky results, cast to an int
+				// ================================= IMAGE OUTPUT =================================
+				for (int i = 0; i < height; i++)
+					for (int j = 0; j < width; j++)
+						img1 << (char)((int)mag[i][j]);
+
+				// Outputting the points stored in the vector to img2
+				int k = 0;
+				for (int i = 0; i < v.size(); i++)
+				{
+					while(k++ != (v.at(i)->x * height + v.at(i)->y - 1))
+						img2 << (char)(0);
+
+					img2 << (char)(255);
+				}
+
+
+				// Output the 'final' matrix to img1
+				for (int i = 0; i < height; i++)
+					for (int j = 0; j < width; j++)
+						img3 << (char)((int)final[i][j]);		
+
+				auto end = std::chrono::high_resolution_clock::now();
+				auto tiempoTotals = std::chrono::duration_cast<std::chrono::seconds>(end-start);
+				auto tiempoTotalm = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+
+				// cout << "Tiempo en segundos " << tiempoTotals.count() << endl;
+				// cout << "Tiempo total en milisegundos " << tiempoTotalm.count() << endl;
+				auto tiempoTotalv = std::chrono::duration_cast<std::chrono::milliseconds>(end-start) - std::chrono::duration_cast<std::chrono::seconds>(end-start);
+
+				cout << ::hi << "\t\t    " << ::sig << "\t\t    " << tiempoTotals.count() << "," << tiempoTotalv.count() << " segundos" << endl;
+				//cout << "Threshold " << z << "\tSigma value " << t << "\tTiempo de ejecución " << tiempoTotals.count() << "," << tiempoTotalv.count() << " segundos" << endl;
+			}
+			catch(const exception& er){
+				continue;
+			}
+		}
 	}
-
-	// Reading in the input image as integers
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
-			pic[i][j] = (int)infile.get();
-
-	// Create the magniute matrix
-	magnitude_matrix(pic, mag, x, y);
-
-	// Get all the peaks and store them in vector
-	HashMap *peaks = new HashMap();
-	vector<Point*> v = peak_detection(mag, peaks, x, y);
-
-	// Go through the vector and call the recursive function and each point. If the value
-	// in the mag matrix is hi, then immediately accept it in final. If lo, then immediately
-	// reject. If between lo and hi, then check if it's next to a hi pixel using recursion
-	HashMap *h = new HashMap();
-	int a, b;
-	for (int i = 0; i < v.size(); i++)
-	{
-		a = v.at(i)->x;
-		b = v.at(i)->y;
-
-		if (mag[a][b] >= hi)
-			final[a][b] = 255;
-		else if (mag[a][b] < lo)
-			final[a][b] = 0;
-		else
-			recursiveDT(mag, final, h, peaks, a, b, 0);
-	}
-
-	// ================================= IMAGE OUTPUT =================================
-	// Outputting the 'mag' matrix to img1. It's very important to cast it to a char.
-	// To make sure that the decimal doesn't produce any wonky results, cast to an int
-	// ================================= IMAGE OUTPUT =================================
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
-			img1 << (char)((int)mag[i][j]);
-
-	// Outputting the points stored in the vector to img2
-	int k = 0;
-	for (int i = 0; i < v.size(); i++)
-	{
-		while(k++ != (v.at(i)->x * height + v.at(i)->y - 1))
-			img2 << (char)(0);
-
-		img2 << (char)(255);
-	}
-
-
-	// Output the 'final' matrix to img1
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
-			img3 << (char)((int)final[i][j]);		
-
-	auto end = std::chrono::high_resolution_clock::now();
-	auto tiempoTotals = std::chrono::duration_cast<std::chrono::seconds>(end-start);
-	auto tiempoTotalm = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-
-	cout << "Tiempo en segundos " << tiempoTotals.count() << endl;
-	cout << "Tiempo total en milisegundos " << tiempoTotalm.count() << endl;
-	auto tiempoTotalv = std::chrono::duration_cast<std::chrono::milliseconds>(end-start) - std::chrono::duration_cast<std::chrono::seconds>(end-start);
-
-
-	cout << "\nTiempo total de ejecución: " << tiempoTotals.count() << "," << tiempoTotalv.count() << " segundos" << endl;
 	return 0;
 }
